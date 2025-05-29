efe4c8fa5fa16491150373c7f139bc4d04442e34
Sofía Sacconi <sofiasacconi650@gmail.com>
2025-05-29 01:05:14 -0300

    insertando carrito js

--- pages/JavaScript/tienda.js
+++ pages/JavaScript/tienda.js
@@ -7,9 +7,14 @@ const total = document.getElementById("total");
 //Función para obtener precio
 function obtenerPrecio(boton){
     const divProducto = boton.closest(".producto");
-    const textoPrecio = divProducto.getElementsByClass("precio")[1].textContent;
-    const precio = parseFloat(textoPrecio);
-    return precio;
+    const textoPrecio = divProducto.querySelectorAll("p")[1].textContent;
+    const precio = parseFloat(
+    textoPrecio
+      .replace("$", "")       // Quita el símbolo $
+      .replace(/\./g, "")     // Quita los puntos (miles)
+      .replace(",", ".")      // Cambia la coma por punto
+    );
+    return isNaN(precio) ? 0 : precio; // Fallback si falla
 }
 
 botones.forEach((boton)=>{
